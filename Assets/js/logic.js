d8f4c8f7b17f3a93361da0944ad9e28539fc854e
Rizwan <rizwanrensa@gmail.com>
2019-07-09 19:40:40 -0400

    weather api implementation

--- Assets/js/logic.js
+++ Assets/js/logic.js
@@ -1,43 +1,51 @@
-
 //On-click event handler (clear-button)
-$("#clear-all").on("click", clear);
+$(document).ready(function() {
+  $('#clear-all').on('click', clear);
 
-//Clear function
-function clear() {
-    $("#map-container-google-1").empty();
-    
-}
+  //Clear function
+  function clear() {
+    $('#map-container-google-1').empty();
+  }
+  console.log('hi');
+  //-------------------------------------------------------------------------
 
-
-//-------------------------------------------------------------------------
-
-//Geting data from the weather api
-
-$("#run-search").click(function(event){
-
+  //Geting data from the weather api
+  $('#run-search').on('click', function(event) {
+    console.log('clicked');
     event.preventDefault();
-    
-    var apiKey="20f50484ba7080a02c3ba933dcafcc36";
-    var zip=$("#zip").val().trim();
-    var city="Toronto";
-    var Country="Canada";
-    var queryUrl= "https://api.openweathermap.org/data/2.5/forecast?q="+city+","+Country+"&zip="+zip+"&appid="+apiKey;
-      
+    var state = 'Ontario';
+
+    var country = 'Canada';
+    console.log(country);
+    var queryURL = `http://api.worldweatheronline.com/premium/v1/weather.ashx?key=6967235c37d54f578f004147190807&q=${state},%20${country}&format=json&num_of_days=14`;
 
     $.ajax({
-        url: queryUrl,
-        method:"GET",
-    }).then(function(response){
+      url: queryURL,
+      method: 'GET'
+    }).then(function(response) {
+      console.log(response);
 
-            console.log(queryUrl);
-            var temp=response.list[0].main.temp;               // temperture 
-            var weather=response.list[0].weather[0].main;       // weather: it is rainy, clear,..
+      var weather = response.data.weather;
+      var arrayOfWeatherChances = [];
 
-            console.log(temp);
-            console.log(weather);
-
+      console.log(weather);
+      for (var i = 0; i < weather.length; i++) {
+        var hourly = weather[i].hourly;
+        var date = response.data.weather[i].date;
+        console.log(hourly, date);
+        var totalChancesOfRainPerDay = 0;
+        for (var k = 0; k < hourly.length; k++) {
+          var chanceOfRain = hourly[k].chanceofrain;
+          totalChancesOfRainPerDay += parseInt(chanceOfRain);
+          // totalChancesOfRain += parseInt(chanceOfRain)
+          // var averageTotalChancesOfRain = totalChancesOfRain / 8
+          // console.log(averageTotalChancesOfRain)
+        }
+        arrayOfWeatherChances.push(totalChancesOfRainPerDay / hourly.length);
+        console.log(arrayOfWeatherChances);
+      }
+      // Calculate average for th
     });
-
+  });
+  //-------------------------------------------------------------------------
 });
-
-//-------------------------------------------------------------------------
