56d79c20f6c1732c862ebab4d82df8236548626f
Fred M <farzad.moghbel@gmail.com>
2019-07-13 21:08:42 -0400

    Google News API+ corressponding changes in HTML

--- Assets/js/logic.js
+++ Assets/js/logic.js
@@ -12,12 +12,12 @@
 var latp = 43.6534;
 var lngp = -79.384;
 
-$(document).ready(function() {
+$(document).ready(function () {
   //---------------------------------------------------
   var arrayOfRainChances = [];
   var arrayOfDate = [];
   //Geting data from the weather api
-  $('#run-search').on('click', function(event) {
+  $('#run-search').on('click', function (event) {
     console.log('clicked');
     event.preventDefault();
 
@@ -38,7 +38,7 @@ $(document).ready(function() {
     $.ajax({
       url: queryURL,
       method: 'GET'
-    }).then(function(response) {
+    }).then(function (response) {
       console.log(response);
 
       var weather = response.data.weather;
@@ -63,7 +63,7 @@ $(document).ready(function() {
 
         chancesOfRainChart(arrayOfDate, arrayOfRainChances);
 
-        //
+
       }
     });
 
@@ -73,29 +73,27 @@ $(document).ready(function() {
       var canvas = document.getElementById('myChart');
       var data = {
         labels: [...arrayOfDate],
-        datasets: [
-          {
-            label: '% Chances of Rain',
-            fill: true,
-            lineTension: 0.1,
-            backgroundColor: 'rgba(75,192,192,0.4)',
-            borderColor: 'rgba(75,192,192,1)',
-            borderCapStyle: 'butt',
-            borderDash: [],
-            borderDashOffset: 0.0,
-            borderJoinStyle: 'miter',
-            pointBorderColor: 'rgba(75,192,192,1)',
-            pointBackgroundColor: '#fff',
-            pointBorderWidth: 1,
-            pointHoverRadius: 0,
-            pointHoverBackgroundColor: 'rgba(75,192,192,1)',
-            pointHoverBorderColor: 'rgba(220,220,220,1)',
-            pointHoverBorderWidth: 2,
-            pointRadius: 5,
-            pointHitRadius: 10,
-            data: [...arrayOfRainChances]
-          }
-        ]
+        datasets: [{
+          label: '% Chances of Rain',
+          fill: true,
+          lineTension: 0.1,
+          backgroundColor: 'rgba(75,192,192,0.4)',
+          borderColor: 'rgba(75,192,192,1)',
+          borderCapStyle: 'butt',
+          borderDash: [],
+          borderDashOffset: 0.0,
+          borderJoinStyle: 'miter',
+          pointBorderColor: 'rgba(75,192,192,1)',
+          pointBackgroundColor: '#fff',
+          pointBorderWidth: 1,
+          pointHoverRadius: 0,
+          pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+          pointHoverBorderColor: 'rgba(220,220,220,1)',
+          pointHoverBorderWidth: 2,
+          pointRadius: 5,
+          pointHitRadius: 10,
+          data: [...arrayOfRainChances]
+        }]
       };
 
       function adddata() {
@@ -136,7 +134,7 @@ $(document).ready(function() {
     $.ajax({
       url: queryURL,
       method: 'GET'
-    }).then(function(response) {
+    }).then(function (response) {
       console.log(response);
 
       latp = response.results[0].geometry.location.lat;
@@ -144,20 +142,50 @@ $(document).ready(function() {
       initMap();
     });
   });
+  //-------------------------------------------------------------------------
+  // Google NEWS API
+  var url = 'https://newsapi.org/v2/everything?' +
+    'q=gardening&' +
+    'from=2019-07-13&' +
+    'sortBy=popularity&' +
+    'apiKey=e89f01e7b8194e1192efb1cde86c7a4d';
 
-  // get lan and lat from the geocoding api and use it in function initmap
+  $.ajax({
+    url: url,
+    method: 'GET'
+  }).then(updatePage)
+  //var articleName = response.articles[0].title;
+  //$("#news").text(articleName);
+
+
+  // var req = new Request(url);
+
+  // fetch(req)
+  //     .then(function(response) {
+  //         console.log(response.json());
+  //         // console.log(response);
+  //     })  
+  // console.log(req);
+  //-----------------------------------------------------------------------------  
 });
 
+// get lan and lat from the geocoding api and use it in function initmap
 function initMap() {
   // The location of Uluru
-  var uluru = { lat: latp, lng: lngp };
+  var uluru = {
+    lat: latp,
+    lng: lngp
+  };
   // The map, centered at Uluru
   var map = new google.maps.Map(document.getElementById('map'), {
     zoom: 15,
     center: uluru
   }); //change the zoom from 4 to 15
   // The marker, positioned at Uluru
-  var marker = new google.maps.Marker({ position: uluru, map: map });
+  var marker = new google.maps.Marker({
+    position: uluru,
+    map: map
+  });
   console.log(map);
 }
 
@@ -208,6 +236,36 @@ function indexAtSeventyAboveChanceOfRainForWeek(chancesOfRainforWeek) {
     }
   }
 }
-//-------------------------------------------------------------------------
 
-// });
+// Function update the "news" section
+
+function updatePage(response) {
+  var numArticles = 3;
+  
+  //var articleName = response.articles[0].title;
+  //$("#news").text(articleName);
+
+  for (var i = 0; i < numArticles; i++) {
+    var article = response.articles[i];
+
+    var $articleList = $("<ul>");
+    $articleList.addClass("list-group");
+
+    //var headline = article.title;
+    var $articleListItem = $("<li class='list-group-item articleHeadline'>");
+
+    var articleName = article.title;
+    $articleListItem.append("<h4>Article Name: " + articleName + "</h4>");
+
+    var author = article.author;
+    $articleListItem.append("<h6>Author: " + author + "</h6>");
+    
+    $articleListItem.append("<a href='" + article.url + "'>" + article.url + "</a>");
+
+    $articleList.append($articleListItem);
+    $("#news").append($articleList);
+  }
+
+  
+}
+
